a2f2316fbcd6f473086a9cb1c8547f2b9c613d6e
Mona-Ali-Mostafa98 <monaali980628@gmail.com>
2024-04-04 20:51:26 +0200

    CRUD Operations of books without validations

--- BooksStore/books/urls.py
+++ BooksStore/books/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from books.views import (index, show)
+from books.views import (home, index, show, create, edit, delete)
 
 urlpatterns = [
+    path('', home, name='books.home'),
     path('index', index, name='books.index'),
-    path('<int:id>', show, name='books.show')
+    path('create', create, name='books.create'),
+    path('<int:id>/show', show, name='books.show'),
+    path('<int:id>/edit', edit, name='books.edit'),
+    path('<int:id>/delete', delete, name='books.delete')
 ]
+
--- BooksStore/books/views.py
+++ BooksStore/books/views.py
@@ -1,6 +1,15 @@
+#django imports
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib import messages
 
-# Create your views here.
+# python imports
+import json
+
+# imports from your created files
+from books.models import Book
+
 books = [
     {"id":1, "image":"book1.png", "title":"Book #1", "category":"science", "author":"Author Name", "price":"100", "pagesNo":"20", "ISBN":"10"},
     {"id":2, "image":"book2.png", "title":"Book #2", "category":"Programing", "author":"Author Name", "price":"200", "pagesNo":"50", "ISBN":"20"},
@@ -10,11 +19,106 @@ books = [
     {"id":6, "image":"book3.png", "title":"Book #6", "category":"Programing", "author":"Author Name", "price":"600", "pagesNo":"600", "ISBN":"60"},
 ];
 
+def home(request):
+    books = Book.objects.all()
+    return render(request, "books/index.html",
+                  context={"books": books}, status=200)
+
 
 def index(request):
-    return render(request, "books/index.html", context = {"books": books}, status=200)
+    books = Book.objects.all()
+    return render(request, "books/crud/index.html",
+                  context={"books": books}, status=200)
+
+def show(request, id):
+    # book = Book.objects.get(id=id)
+    book = get_object_or_404(Book, pk=id)
+    return render(request, "books/crud/show.html", context={"book": book})
 
 
+def delete(request, id):
+    book = get_object_or_404(Book, pk=id)
+    book.delete()
+    url = reverse("books.index")
+    messages.success(request, "Book Deleted Successfully.")
+
+    return redirect(url)
+
+"""
+def band_delete(request, id):
+    band = Band.objects.get(id=id)  # we need this for both GET and POST
+
+    if request.method == 'POST':
+        # delete the band from the database
+        band.delete()
+        # redirect to the bands list
+        return redirect('band_list'))
+
+    # no need for an `else` here. If it's a GET request, just continue
+
+    return render(request,
+                    'listings/band_delete.html',
+                    {'band': band})"""
+
+
+def create(request):
+    if request.method == "POST":
+        if request.FILES:
+            image = request.FILES["image"]
+        else:
+            image = None
+
+        #print(request.POST)
+
+        title = request.POST["title"]
+        category = request.POST["category"]
+        author = request.POST["author"]
+        price = request.POST["price"]
+        pagesNo = request.POST["pagesNo"]
+        ISBN = request.POST["ISBN"]
+
+        book = Book(
+            title=title, category=category, author=author,  price=price, pagesNo=pagesNo, ISBN=ISBN, image=image
+        )
+        book.save()
+        # messages.add_message(request, messages.INFO, "Book Add Successfully")
+        messages.success(request, "Book Add Successfully.")
+
+        return redirect(book.show_url)
+
+    # get request
+    return  render(request, 'books/crud/create.html')
+
+
+def edit(request, id):
+    book = get_object_or_404(Book, pk=id)
+
+    if request.method == "POST":
+        if "image" in request.FILES:
+            image = request.FILES["image"]
+            book.image = image
+
+        title = request.POST.get("title")
+        category = request.POST.get("category")
+        author = request.POST.get("author")
+        price = request.POST.get("price")
+        pagesNo = request.POST.get("pagesNo")
+        ISBN = request.POST.get("ISBN")
+
+        book.title = title
+        book.category = category
+        book.author = author
+        book.price = price
+        book.pagesNo = pagesNo
+        book.ISBN = ISBN
+        book.save()
+
+        messages.success(request, "Book updated successfully.")
+        return redirect(book.show_url)
+
+    return render(request, 'books/crud/edit.html', {'book': book})
+
+"""
 def show(request, id):
     filtered_books = filter(lambda book: book['id'] == id, books)
     filtered_books = list(filtered_books)
@@ -25,3 +129,4 @@ def show(request, id):
         })
 
     return HttpResponse("book not found")
+"""
