847b874c02c8c0ec2711f56aaafe83adce9bb63b
Mona-Ali-Mostafa98 <monaali980628@gmail.com>
2024-04-04 21:41:13 +0200

    Sent contact us message

--- BooksStore/contacts/models.py
+++ BooksStore/contacts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.shortcuts import  reverse
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    email = models.EmailField()
+    subject = models.CharField(max_length=100, null=True, blank=True)
+    message = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+
+    """ object returned with name"""
+    def __str__(self):
+        return f'{self.title}'
+
+    @property
+    def index_url(self):
+        pass
+        url = reverse('books.home')
+        return url
--- BooksStore/contacts/views.py
+++ BooksStore/contacts/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.shortcuts import  reverse
+from django.contrib import messages
+
+# imports from your created files
+from contacts.models import Contact
 
 # Create your views here.
-def contact(request):
-    return render(request, "contacts/index.html", status=200)
+# def contact(request):
+#     return render(request, "contacts/index.html", status=200)
 
+def contact(request):
+    if request.method == "POST":
+        name = request.POST["name"]
+        email = request.POST["email"]
+        subject = request.POST["subject"]
+        message = request.POST["message"]
+
+        contact = Contact(name=name, email=email, subject=subject,  message=message)
+        contact.save()
+        messages.success(request, "Your Message Sent Successfully.")
+
+        return redirect(contact.index_url)
+
+    # get request
+    return render(request, "contacts/index.html", status=200)
